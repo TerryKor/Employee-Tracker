3426d9622ce54785ef7337146ecff176a68ca23b
Terry <misterfreemann@gmail.com>
2023-03-17 11:27:27 -0400

    added functions veiwRole,viewAllEmployees,viewAllDepartments and provide choices to user

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const mysql = require("mysql2");
 const inquire = require("inquirer");
 const consoleTable = require("console.table");
+const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 const db = mysql.createConnection({
   host: "localhost",
@@ -15,18 +21,17 @@ const allDepartments = []
 const allRoles = []
 const allManagers = []
 const choicesArray = [
-  "What would you like to do?",
   "View all employees",
   "Add employee",
   "View all roles",
   "Add role",
   "View all departments",
   "Add department",
-
+  "Exit"
 ];
 
 
-const addDepartment = [
+const addDepartmentQuestions = [
   { type: "input", name: "name", message: "What is the name of the department ?" },
 ]
 
@@ -34,7 +39,7 @@ const addDepartment = [
 
 
 
-const addRole = [
+const addRoleQuestions = [
   { type: "input", name: "name", message: "What is role name?" },
   { type: "input", name: "salary", message: "What is role salary?" },
   {
@@ -47,7 +52,7 @@ const addRole = [
 
 
 
-const addEmployee = [
+const addEmployeeQuestions = [
   { type: "input", name: "name", message: "What is employee's first name?" },
   { type: "input", name: "salary", message: "What is employee's last name?" },
   {
@@ -68,13 +73,47 @@ const menuQuestions = [
   {
     type: "list",
     name: "chosenOption",
-    message: "Would you like to add another team member?",
+    message: "What would you like to do?",
     choices: choicesArray,
   },
 ];
 
-async function init(){
 
+
+async function init(){
+  db.query('SELECT * FROM department', function (err, results) {
+    results.forEach((element)=>{
+      allDepartments.push(element.name)
+    })
+  });
+  db.query('SELECT * FROM role', function (err, results) {
+    results.forEach((element)=>{
+      allRoles.push(element.title)
+    })
+  
+  });
+
+  const menuResponses = await inquire.prompt(menuQuestions)
+  if(menuResponses.chosenOption == choicesArray[0]){
+    viewAllEmployees()
+  }
+  else if(menuResponses.chosenOption == choicesArray[1]){
+    addEmployee()
+  }
+  else if(menuResponses.chosenOption == choicesArray[2]){
+    viewAllRoles()
+  }
+  else if(menuResponses.chosenOption == choicesArray[3]){
+    addRole()
+  }
+  else if(menuResponses.chosenOption == choicesArray[4]){
+    viewAllDepartments()
+  }
+  else if(menuResponses.chosenOption == choicesArray[5]){
+    addDepartment()
+  }else{
+    return;
+  }
 
 
 
@@ -84,6 +123,10 @@ async function init(){
 
 
 function viewAllEmployees(){
+  db.query("SELECT * from employee", (err, results)=>{
+    console.table(results)
+  })
+
 
 };
 function addEmployee(){
@@ -94,13 +137,28 @@ function updateEmployeeRole(){
 };
 function viewAllRoles(){
 
-};
-function addRole(){
-
-};
-function viewAllDepartments(){
-
+  db.query("SELECT * from role", (err, results)=>{
+      console.table(results)
+    })
+    
+  };
+  function addRole(){
+  };
+  function viewAllDepartments(){
+    db.query("SELECT * from department", (err, results)=>{
+        console.table(results)
+      })
+    
 };
 function addDepartment(){
 
 };
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+
+
+init();
+
